c849e52c65244db6608129aa0057bce006a9df38
flaredragon <akashdeepramnaney@gmail.com>
2017-06-21 05:42:50 +0530

    Update x.js

--- x.js
+++ x.js
@@ -1,11 +1,10 @@
 try{var x = document.getElementById("demo").innerHTML;
 document.getElementById("demo").style.visibility = "hidden";   
    }
-catch(err) {
-        var a=err;
-    }
-if(x!=null)
-  var y=x;
-if(x)
-  var y=x;
-document.getElementById("f").innerHTML=y;
+catch(err)
+{var a =err;
+ 
+}  
+if (typeof x != 'undefined')
+{document.getElementById("t").innerHTML=x;
+   }
